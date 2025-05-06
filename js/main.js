d24ccb46c3bb98165eae6b8249069849c6216e76
Gregorio <gregorio.galindo97@gmail.com>
2025-05-06 17:15:32 -0600

    Se hace el navbar flotante

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ let btnAcrilico=document.getElementById("acrilico");
 let btnGelish=document.getElementById("gelish");
 let btnRubigel=document.getElementById("rubigel");
 
-btnAcrilico.addEventListener('click', function() {
+btnAcrilico.addEventListener('click', function(e) {
     // Puedes agregar condiciones o lógica aquí
-    window.location.href = '../';
+    e.preventDefault();
+    window.location.href = '../acrilico.html';
+});
+btnGelish.addEventListener('click', function(e) {
+    // Puedes agregar condiciones o lógica aquí
+    e.preventDefault();
+    window.location.href = '../gelish.html';
+});
+btnRubigel.addEventListener('click', function(e) {
+    // Puedes agregar condiciones o lógica aquí
+    e.preventDefault();
+    window.location.href = '../rubigel.html';
 });
