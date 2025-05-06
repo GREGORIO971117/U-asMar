c116e225c2b75b49fab43b01a5a425fca95df87b
Gregorio <gregorio.galindo97@gmail.com>
2025-05-06 10:47:46 -0600

    Se suben archivos iniciales del proyecto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let btnAcrilico=document.getElementById("acrilico");
+let btnGelish=document.getElementById("gelish");
+let btnRubigel=document.getElementById("rubigel");
+
+btnAcrilico.addEventListener('click', function() {
+    // Puedes agregar condiciones o lógica aquí
+    window.location.href = '../';
+});
