764dec0bcd5db6562855ade833a0c856be71bd12
Gregorio <gregorio.galindo97@gmail.com>
2025-05-08 17:04:46 -0600

    Se agrega boton ver mas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-let btnAcrilico=document.getElementById("acrilico");
 let btnGelish=document.getElementById("gelish");
 let btnRubigel=document.getElementById("rubigel");
 
-btnAcrilico.addEventListener('click', function(e) {
-    // Puedes agregar condiciones o lógica aquí
-    e.preventDefault();
-    window.location.href = './acrilico.html';
-});
 btnGelish.addEventListener('click', function(e) {
     // Puedes agregar condiciones o lógica aquí
     e.preventDefault();
