3201158fb63a700bebbf322ff7804edf181ea6e7
Gregorio <gregorio.galindo97@gmail.com>
2025-05-21 22:41:37 -0600

    Se cambia imagen de portada se agregan servicios

---
+++ assets/js/cardSoftgel.js
@@ -0,0 +1,50 @@
+// Datos del contenido de las uñas
+const contenidoUñasData = [
+  {
+    imagenSrc: "./assets/images/Softgel.jpeg",
+    nombre:"Colores rosas",
+    descripcion: "Nivelación con ruber, esmaltado color nude.",
+  },
+  {
+    imagenSrc: "./assets/images/Softgel_1.jpeg",
+    nombre:"Colores rosas",
+    descripcion: "Nivelación con ruber, esmaltado color nude.",
+  },
+  // Puedes agregar más objetos para más secciones
+];
+
+// Obtener el contenedor
+const contenedorUñas = document.getElementById('contenedorUñas');
+
+// Generar el contenido dinámicamente usando innerHTML
+let contenidoHTML = '';
+
+contenidoUñasData.forEach(item => {
+  contenidoHTML += `
+    <div class="row">
+      <div class="col-sm-12 col-md-6 mb-4">
+        <div class="card">
+          <img
+            src="${item.imagenSrc}"
+            class="card-img-top rounded-circle mx-auto d-block"
+            alt="${item.nombre}"
+          />
+        </div>
+      </div>
+      <div
+        class="col-sm-12 col-md-6 mb-4 align-items-center justify-content parrafo"
+        style="
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          height: 100%;
+        "
+      >
+        <h2>${item.nombre}</h2>
+        <p>${item.descripcion}</p>
+      </div>
+    </div>
+  `;
+});
+
+contenedorUñas.innerHTML = contenidoHTML;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 let btnAcrilico=document.getElementById("acrilico");
 let btnGelish=document.getElementById("gelish");
 let btnRubigel=document.getElementById("rubergel");
+let btnSoftgel=document.getElementById("softgel");
+let btnpolygel=document.getElementById("polygel");
+
 
 btnAcrilico.addEventListener('click', function(e) {
     // Puedes agregar condiciones o lógica aquí
@@ -16,5 +19,15 @@ btnRubigel.addEventListener('click', function(e) {
     // Puedes agregar condiciones o lógica aquí
     e.preventDefault();
     window.location.href = './ruberGel.html';
+});
+btnSoftgel.addEventListener('click', function(e) {
+    // Puedes agregar condiciones o lógica aquí
+    e.preventDefault();
+    window.location.href = './softgel.html';
+});
+btnpolygel.addEventListener('click', function(e) {
+    // Puedes agregar condiciones o lógica aquí
+    e.preventDefault();
+    window.location.href = './polygel.html';
 });
 
